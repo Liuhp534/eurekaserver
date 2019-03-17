31bb398655601d0a1c8a24719e481694a3cc3c62
Liuhp534 <617456341@qq.com>
2019-03-17 17:24:47 +0800

    Initial commit

---
+++ src/main/java/liuhp/EurekaApplication.java
@@ -0,0 +1,19 @@
+package liuhp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description: 启动类 a
+ * @author: hz16092620
+ * @create: 2019-03-07 10:56
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
